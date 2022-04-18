31ff94f4cb36d19ebcc2f40d72a1620d2865cd7a
¨Daniel <danielhh.dev@gmail.com>
2022-04-18 15:35:05 -0500

    Javascrip basico

---
+++ funciones.js
@@ -0,0 +1,8 @@
+var x,y;
+function comparar(x,y) {
+    if( x >= y) {
+        alert("Fanny le da unbesito a Danny")
+    }
+}
+
+comparar(8,6)
---
+++ if-else.js
@@ -0,0 +1,36 @@
+//CONDITIONALS
+
+
+if (true) {
+    console.log("Hola");
+} 
+
+//Si se cumple la condición, lo que está dentro de las llaves se ejecuta, ene ste caso al ser siempre verdadero, se imprime en consola la palabra Hola
+
+
+if (false) {
+    console.log("Hola");
+} else {
+    console.log("Soy falso");
+} 
+
+if (x == y) {
+    alert("Fanny le peba a Daniel")
+} else {
+    alert("Fanny le da un besito a Danny")
+}
+
+//Al ser una condición false la sentencia que se ejecuta es la de el ELSE
+
+
+//Varias condiciones
+if (condición1)
+   sentencia1
+else if (condición2)
+   sentencia2
+else if (condición3)
+   sentencia3
+
+// Operadores ternarios
+
+condición ? expr1 : expr2 
